063152e7a0599d8472de621214b3a5905c7f6ccd
aNdrew <alex.alex959.aa@gmail.com>
2017-11-29 05:02:15 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -7,24 +7,23 @@ vRPclient = Tunnel.getInterface("vRP","vRP_empresas")
 
 RegisterServerEvent('business:salary')
 AddEventHandler('business:salary', function()
-  	local user_id = vRP.getUserId(source)
-	if vRP.hasPermission(user_id,"business") then
-		vRP.giveMoney(user_id,26400)
-		vRPclient.notify(source,{"Your business earnings: $26400"})
+  	local user_id = vRP.getUserId({source})
+	if vRP.hasGroup({user_id,'HBO'}) then
+		vRP.giveMoney({user_id,26400})
 	end																														
 end)
 
-RegisterServerEvent('hbo:checkmoney')
-AddEventHandler('hbo:checkmoney', function(empresa)
+RegisterServerEvent('business:checkmoney')
+AddEventHandler('business:checkmoney', function()
 	local user_id = vRP.getUserId({source})
 	local player = vRP.getUserSource({user_id})
-	if vRP.hasGroup({user_id,"HBO"}) then
-	TriggerClientEvent('hbo:alreadyin', player)
+	if vRP.hasGroup({user_id,'HBO'}) then
+	TriggerClientEvent('business:alreadyin', player)
 	else if vRP.tryFullPayment({user_id,1320000}) then
-		vRP.addUserGroup({user_id,"HBO"})
-		TriggerClientEvent('hbo:success', player)
+		vRP.addUserGroup({user_id,'HBO'})
+		TriggerClientEvent('business:success', player)
 	  else
-		TriggerClientEvent('hbo:notenough', player)
+		TriggerClientEvent('business:notenough', player)
 	  end
 	 end
 end)
